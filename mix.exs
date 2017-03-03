6c6a4d53e477f523cc42de5027619d6ecd89f30f
Zach Wentz <zwentz@netflix.com>
2017-03-02 23:23:47 -0800

    removed deprecation warnings

--- mix.exs
+++ mix.exs
@@ -5,11 +5,11 @@ defmodule MobileDoc.Mixfile do
     [app: :mobiledoc,
      version: "0.0.1",
      elixir: "~> 1.0",
-     description: description,
-     package: package,
+     description: description(),
+     package: package(),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps()]
   end
 
   # Configuration for the OTP application
